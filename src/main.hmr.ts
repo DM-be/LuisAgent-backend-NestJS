1f241cd76ee06f47051eecb9a1b24423ed56d4b2
MorentDennis <dennis.morent@student.howest.be>
2018-10-03 20:34:32 +0200

    updated the port....

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 declare const module: any;
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
