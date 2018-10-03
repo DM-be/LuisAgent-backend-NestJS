c5e70c196dff896af47a2f3f5a6b9e2284cce736
MorentDennis <dennis.morent@student.howest.be>
2018-10-03 20:37:41 +0200

    fix

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -2,12 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 declare const module: any;
-const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
     module.hot.accept();
